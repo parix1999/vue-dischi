05950ee539baafc6e53d747aeffe724655455422
parix1999 <parisottoedoardo@gmail.com>
2021-07-14 17:20:59 +0200

    modifiche style

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -43,16 +43,33 @@ export default {
 <style lang="scss" scoped>
 
     .bg-cards {
-        background: #2e3a46;
-
+        width: calc(100% / 5);
+        text-align: center;
+        .inner {
+            background: #2e3a46;
+            height:400px;
+            margin-top:24px;
+            padding: 15px;
+        }
     }
     .box-pic-song {
         width:200px;
+        margin:0 auto;
         img {
             width:100%;
         }
     }
-
+    .titolo {
+        color:white;
+        font-size: 30px;
+        font-weight: bold;
+    }
+    .autore{
+        color: #7e8080;
+    }
+    .anno{
+        color: #7e8080;
+    }
 
 
 </style>
